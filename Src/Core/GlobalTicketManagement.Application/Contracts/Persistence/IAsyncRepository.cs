9124ba9583f834f449493311bd7945451fd5d260
rahimlotfi1368 <sinuhe_1368@yahoo.com>
2024-01-01 01:00:36 +0330

    Add project files.

---
+++ Src/Core/GlobalTicketManagement.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicketManagement.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetListAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+}
---
+++ Src/Core/GlobalTicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using GlobalTicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicketManagement.Application.Contracts.Persistence
+{
+    public interface ICategoryRepository:IAsyncRepository<Category>
+    {
+    }
+}
