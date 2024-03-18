7108195dce13af3ab0cfed3cb371850534f68f2f
rahimlotfi1368 <sinuhe_1368@yahoo.com>
2024-03-19 01:25:32 +0330

    remove unnessery usings from class

--- Src/Core/GlobalTicketManagement.Application/Contracts/Persistence/IEventRepository.cs
+++ Src/Core/GlobalTicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -1,9 +1,4 @@
 ﻿using GlobalTicketManagement.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GlobalTicketManagement.Application.Contracts.Persistence
 {
